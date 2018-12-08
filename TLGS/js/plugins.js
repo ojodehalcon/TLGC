b4181c47310cc0621857827b375d8c3c0b631bae
KIRADARK <44528974+Kiradarkxd@users.noreply.github.com>
2018-12-08 13:50:56 -0600

    ver 2.1.5

--- TLGS/js/plugins.js
+++ TLGS/js/plugins.js
@@ -4,6 +4,7 @@ var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"Basic plugin for manipulating important parameters.","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
 {"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"true","Custom Image":"Veritron games1","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"MadeWithMv","status":true,"description":"Show a Splash Screen \"Made with MV\" and/or a Custom Splash Screen before going to main screen.","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"true","Custom Image":"","Fade Out Time":"","Fade In Time":"","Wait Time":""}},
 {"name":"AltMenuScreen","status":true,"description":"Alternative menu screen layout.","parameters":{}},
 {"name":"AltSaveScreen","status":false,"description":"Alternative save/load screen layout.","parameters":{}},
 {"name":"EnemyBook","status":false,"description":"Displays detailed statuses of enemies.","parameters":{"Unknown Data":"??????"}},
